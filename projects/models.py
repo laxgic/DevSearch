b968bbd3bbb7ad33798501ecd8ed114a6b043041
mogiczo <mogiczo@gmail.com>
2021-12-06 09:22:11 -0800

    Add the Project, Tag and Review models to projects app

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    demo_link = models.CharField(max_length=2000, blank=True)
+    source_link = models.CharField(max_length=2000, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True,
+                          editable=False, unique=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote')
+    )
+    # TODO
+    # owner =
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True,
+                          editable=False, unique=True)
+    
+    def __str__(self):
+        return self.name
+
